fec1200af8269954e37a99de2a4d7979833d8653
MAHMOUD <mahmoud.mostafa20112011@gmail.com>
2022-05-31 23:32:23 +0200

    update6

--- virtual_fitting.py
+++ virtual_fitting.py
@@ -4,13 +4,14 @@ import numpy as np
 import io
 from PIL import Image, ImageFile
 
-person_image = "mms_front.jpeg"
+model_image = "model.jpg"
 cloth_image = "cloth_image.png"
+category = "t-shirt"
 
 
-def remove_bg(person_img, cloth_img):
-    #remove background from person image
-    ImageFile.LOAD_TRUNCATED_IMAGES = True
+def remove_bg(cloth_img):
+    # remove background from person image
+    '''ImageFile.LOAD_TRUNCATED_IMAGES = True
     f = np.fromfile(person_img)
     result_p = remove(f)
     img_p = Image.open(io.BytesIO(result_p)).convert("RGBA")
@@ -18,16 +19,16 @@ def remove_bg(person_img, cloth_img):
     p_no_bg = cv2.imread("person_no_bg.png", cv2.IMREAD_UNCHANGED)
     p_no_alpha = cv2.imread("person_no_bg.png")
 
-    #check if person image is rotated
+    # check if person image is rotated
     (height, width, channel) = p_no_alpha.shape
     if height < width:
         p_no_alpha = cv2.rotate(p_no_alpha, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-    #get person mask
-    p_mask, p_start, p_end = convert_crop(p_no_alpha)
-    cv2.imwrite("person mask.png", p_mask)
+    # get person mask
+    p_mask, p_start, p_end = convert_person(p_no_alpha)
+    cv2.imwrite("person mask.png", p_mask)'''
 
-    #remove background from clothes image
+    # remove background from clothes image
     ImageFile.LOAD_TRUNCATED_IMAGES = True
     f = np.fromfile(cloth_img)
     result_c = remove(f)
@@ -36,34 +37,46 @@ def remove_bg(person_img, cloth_img):
     c_no_bg = cv2.imread("cloth_no_bg.png", cv2.IMREAD_UNCHANGED)
     c_no_alpha = cv2.imread("cloth_no_bg.png")
 
-    #check if image is rotated
+    # check if image is rotated
     (height, width, channel) = c_no_alpha.shape
     if height < width:
         c_no_alpha = cv2.rotate(c_no_alpha, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-    #get cloth mask
-    c_mask, c_start, c_end = convert_crop(c_no_alpha)
-    cv2.imwrite("cloth mask.png", c_mask)
+    # get cloth mask
+    c_mask, s_t, s_b, s_l, s_r = convert_crop_cloth(c_no_alpha)
+    c_no_bg = c_no_bg[s_t:s_b, s_l:s_r]
 
-    return p_no_bg, p_mask, p_start, p_end, c_no_bg, c_mask, c_start, c_end
+    return c_no_bg, c_mask
 
 
-def convert_crop(img):
-    W = img.shape[1]
+def convert_person(img):
     # convert image to black and white
     black_pixels_mask = np.all(img == [0, 0, 0], axis=-1)
     non_black_pixels_mask = np.any(img != [0, 0, 0], axis=-1)
     img[black_pixels_mask] = [255, 255, 255]
     img[non_black_pixels_mask] = [0, 0, 0]
-    h1 = crop(img)
-    h2 = crop_2(img)
-    #img_mask = img[h1:h2, 0:W]
+    # img_mask = img[h1:h2, 0:W]
 
-    return img, h1, h2
+    return img
+
+
+def convert_crop_cloth(img):
+    # convert image to black and white
+    black_pixels_mask = np.all(img == [0, 0, 0], axis=-1)
+    non_black_pixels_mask = np.any(img != [0, 0, 0], axis=-1)
+    img[black_pixels_mask] = [255, 255, 255]
+    img[non_black_pixels_mask] = [0, 0, 0]
+    s_t = crop_top(img)
+    s_b = crop_bottom(img)
+    s_l = crop_left(img)
+    s_r = crop_right(img)
+    img_mask = img[s_t:s_b, s_l:s_r]
+
+    return img_mask, s_t, s_b, s_l, s_r
 
 
 # crop image from top
-def crop(converted_image):
+def crop_top(converted_image):
     for h in range(converted_image.shape[0] - 1):
         for w in range(int(converted_image.shape[1] / 8), int(converted_image.shape[1] * (7 / 8)), 1):
             if np.any(converted_image[h, w, :] == 0):
@@ -71,137 +84,122 @@ def crop(converted_image):
 
 
 # crop image from bottom
-def crop_2(converted_image):
+def crop_bottom(converted_image):
     for h in range(converted_image.shape[0] - 1, 0, -1):
         for w in range(int(converted_image.shape[1] / 8), int(converted_image.shape[1] * (7 / 8)), 1):
             if np.any(converted_image[h, w, :] == 0):
                 return h
 
 
-def get_person_parts(p_image, p_start, p_end):
-    (_, WIDTH, CHANNEL) = p_image.shape
-    HEIGHT = p_end - p_start
-    # height ration between original height of person & height of image
-    # height_ratio = person_height / HEIGHT
-    # calculate start & end of the 8 parts of human body
-    # part1_start = 0
-    part1_end = round(p_start + HEIGHT * (1 / 8))
-
-    part2_start = part1_end + 1
-    part2_end = round(p_start + HEIGHT * (2 / 8))
-
-    part3_start = part2_end + 1
-    part3_end = round(p_start + HEIGHT * (3 / 8))
-
-    part4_start = part3_end + 1
-    part4_end = round(p_start + HEIGHT * (4 / 8))
-
-    part5_start = part4_end + 1
-    part5_end = round(p_start + HEIGHT * (5 / 8))
-
-    # part6_start = part5_end + 1
-    # part6_end = round(p_start + HEIGHT * (6 / 8))
-
-    # part7_start = part6_end + 1
-    # part7_end = round(p_start + HEIGHT * (7 / 8))
-
-    # part8_start = part7_end + 1
-    part8_end = p_end
-
-    # shoulder_line_height = round(part2_start + ((part2_end - part2_start) / 2))
-
-    # calculate needed lengths
-    chest_line_height = round(part3_start + ((part3_end - part3_start) / 4))
-    waist_line_height = round(part4_start + ((part4_end - part4_start) / 4))
-    hip_line_height = part5_start
-    upper_body_length = round((part5_start + ((part5_end - part5_start) / 2)) - \
-                              (part2_start + ((part2_end - part2_start) / 3)))
-    inside_leg_length = round(part8_end - (part5_start + ((part5_end - part5_start) / 2)))
-
-    chest_line_length = 0
-    for i in range(WIDTH - 1):
-        if np.any(p_image[chest_line_height, i, :] == 0):
-            chest_line_length = chest_line_length + 1
-
-    waist_line_length = 0
-    for i in range(WIDTH - 1):
-        if np.any(p_image[waist_line_height, i, :] == 0):
-            waist_line_length = waist_line_length + 1
-
-    hip_line_length = 0
-    for i in range(WIDTH - 1):
-        if np.any(p_image[hip_line_height, i, :] == 0):
-            hip_line_length = hip_line_length + 1
-            if hip_line_length == 1:
-                hip_start = i
-
-    '''chest_line_length *= height_ratio
-    waist_line_length *= height_ratio
-    upper_body_length *= height_ratio
-    inside_leg_length *= height_ratio
-    hip_line_length *= height_ratio'''
-    return chest_line_length, waist_line_length, hip_line_length, upper_body_length, inside_leg_length, hip_line_height, hip_start
+# crop image from left
+def crop_left(converted_image):
+    for w in range(converted_image.shape[1] - 1):
+        for h in range(converted_image.shape[0]):
+            if np.any(converted_image[h, w, :] == 0):
+                return w
 
 
-def get_cloth_parts(c_image, c_start, c_end):
-    (_, W, D) = c_image.shape
-    H = c_end - c_start
-    # get waist & hip height for upper clothes
-    waist_height = round(c_start + H * 7 / 10)
-    hip_height = round(c_start + H * 9 / 10)
-    # get average of waist & hip for lower clothes
-    w_h_height = round((waist_height + hip_height) / 2)
+# crop image from top
+def crop_right(converted_image):
+    for w in range(converted_image.shape[1] - 1, 0, -1):
+        for h in range(converted_image.shape[0]):
+            if np.any(converted_image[h, w, :] == 0):
+                return w
 
+
+def get_model_parts():
+    hip_height = 425
+    waist_height = 370
+    upper_cloth_start = 160
+    upper_cloth_end = hip_height
+    t_shirt_x = 180
+    shirt_x = 80
+    pants_x = 230
+    t_shirt_width = 420 - 180
+    shirt_width = 525 - 80
+    pants_width = 370 - 230
+    lower_cloth_start = waist_height
+    lower_cloth_end = 685
+    upper_b_length = upper_cloth_end - upper_cloth_start
+    lower_b_length = lower_cloth_end - lower_cloth_start
+
+    return upper_b_length, t_shirt_width, t_shirt_x, shirt_width, shirt_x, lower_b_length, pants_width, pants_x, hip_height, waist_height
+
+
+'''def get_cloth_parts(c_image):
+    (H, W, _) = c_image.shape
+    # get length of clothes
+    cloth_length = H
+    # get hip height & length for upper clothes
+    hip_height = round(H * 9.5 / 10)
+    hip_length = 0
+    for i in range(W - 1):
+        if np.any(c_image[hip_height, i, :] == 0):
+            hip_length = hip_length + 1
+    space_length = W - hip_length
+
+    # get waist height & length for lower clothes
+    waist_height = round(H * 0.5 / 10)
     waist_length = 0
     for i in range(W - 1):
         if np.any(c_image[waist_height, i, :] == 0):
             waist_length = waist_length + 1
 
-    hip_length = 0
-    space_length = 0
-    for i in range(W - 1):
-        if np.any(c_image[hip_height, i, :] == 0):
-            hip_length = hip_length + 1
-        if np.any(c_image[hip_height, i, :] != 0):
-            space_length = space_length + 1
+    return cloth_length, hip_length, space_length, waist_length'''
 
-    w_h_length = 0
-    for i in range(W - 1):
-        if np.any(c_image[w_h_height, i, :] == 0):
-            w_h_length = w_h_length + 1
+def merge_images(model, c_no_bg, start_x, end_x, start_y, end_y):
+    # so let's split the overlay image into its individual channels
+    fg_b, fg_g, fg_r, fg_a = cv2.split(c_no_bg)
+    # Make the range 0...1 instead of 0...255
+    fg_a = fg_a / 255.0
+    # Multiply the RGB channels with the alpha channel
+    label_rgb = cv2.merge([fg_b * fg_a, fg_g * fg_a, fg_r * fg_a])
+    # Work on a part of the background only
+    part_of_bg = model[start_y:end_y, start_x:end_x, :]
+    # Same procedure as before: split the individual channels
+    bg_b, bg_g, bg_r = cv2.split(part_of_bg)
+    # Merge them back with opposite of the alpha channel
+    part_of_bg = cv2.merge([bg_b * (1 - fg_a), bg_g * (1 - fg_a), bg_r * (1 - fg_a)])
+    # Add the label and the part of the background
+    cv2.add(label_rgb, part_of_bg, part_of_bg)
+    # Replace a part of the background
+    model[start_y:end_y, start_x:end_x, :] = part_of_bg
 
-    # get length of lower clothes
-    lower_length = H
-
-    return waist_length, hip_length, space_length, w_h_length, lower_length
+    return model
 
 
-def virtual_fitting(person_image, cloth_image):
-    p_no_bg, p_mask, p_start, p_end, c_no_bg, c_mask, c_start, c_end = remove_bg(person_image, cloth_image)
+def virtual_fitting(model_image, cloth_image, category):
+    model = cv2.imread(model_image)
+    c_no_bg, c_mask = remove_bg(cloth_image)
+    upper_length, t_shirt_width, t_shirt_x, shirt_width, shirt_x, lower_length, pants_width, pants_x, hip_height, waist_height = get_model_parts()
+    if category == "t-shirt":
+        c_no_bg = cv2.resize(c_no_bg, (t_shirt_width, upper_length))
+        start_x = t_shirt_x
+        end_x = start_x + c_no_bg.shape[1]
+        start_y = hip_height - c_no_bg.shape[0]
+        end_y = hip_height
+        model = merge_images(model, c_no_bg, start_x, end_x, start_y, end_y)
 
-    chest_p, waist_p, hip_p, upper_p, insideLeg_p, hip_height, hip_start = get_person_parts(p_mask, p_start, p_end)
-    print(f"hip_p {hip_p} upper_p {upper_p}")
+    elif category == "shirt" or category == "jacket":
+        c_no_bg = cv2.resize(c_no_bg, (shirt_width, upper_length))
+        start_x = shirt_x
+        end_x = start_x + c_no_bg.shape[1]
+        start_y = hip_height - c_no_bg.shape[0]
+        end_y = hip_height
+        model = merge_images(model, c_no_bg, start_x, end_x, start_y, end_y)
 
-    c_no_bg = cv2.resize(c_no_bg, (hip_p, upper_p-10))
+    elif category == "pants" or category == "shorts":
+        c_no_bg = cv2.resize(c_no_bg, (pants_width, lower_length))
+        start_x = pants_x
+        end_x = start_x + c_no_bg.shape[1]
+        start_y = waist_height
+        end_y = waist_height + c_no_bg.shape[0]
+        model = merge_images(model, c_no_bg, start_x, end_x, start_y, end_y)
 
-    waist_c, hip_c, space_length, w_h_c, lower_l_c = get_cloth_parts(c_mask, c_start, c_end)
-
-    new_width = hip_p + space_length
-
-    #c_no_bg = cv2.resize(c_no_bg, (new_width, upper_p))
-
-    start_x = hip_start
-    end_x = hip_start + c_no_bg.shape[1]
-    start_y = hip_height - c_no_bg.shape[0]
-    end_y = hip_height
-    print(f"start x: {start_x} end x: {end_x} start y: {start_y} end y: {end_y}")
-
-    blended_img = cv2.addWeighted(c_no_bg, 1, p_no_bg[start_y:end_y, start_x:end_x, :], 0, 0)
-    p_no_bg[start_y:end_y, start_x:end_x, :] = blended_img
-
-    cv2.imshow("out", p_no_bg)
-    cv2.imwrite("out.png", p_no_bg)
+    cv2.imshow("out", model)
+    cv2.imwrite("out.png", model)
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
-virtual_fitting(person_image=person_image, cloth_image=cloth_image)
+virtual_fitting(model_image=model_image, cloth_image=cloth_image, category=category)
